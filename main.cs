cb515f033b6e58e7d2a487d33335d53fde8b4b6b
RiadCure <100498353+riadPCA@users.noreply.github.com>
2022-03-01 01:10:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   int valor1=10,valor2=2,resultado;
+    resultado=valor1+valor2;
+    Console.WriteLine('El Resultado es: '+resultado);
+  }
+}
